4eea52ed1db3a5bd055372bc0fcdaf27cc52fbae
Pritom Khamaru <51317292+Pritam78459@users.noreply.github.com>
2020-09-15 18:18:39 +0530

    Update practical_1b.py

--- practical_1b.py
+++ practical_1b.py
@@ -9,14 +9,21 @@ Mat3 = [[0,0,0,],
      [0,0,0,],
      [0,0,0,]]
 
+# Matrix Addition
 for i in range(len(Mat1)):
     for j in range(len(Mat2)):
         Mat3[i][j] = Mat1[i][j] + Mat2[i][j]
         
-print(M3)
+print(Mat3)
+
+# Matrix Multiplication
 
 for i in range(len(Mat1)):
     for j in range(len(Mat2)):
         Mat3[i][j] = Mat1[i][j] * Mat2[i][j]
         
-print(M3)
+print(Mat3)
+
+#matrix transpose
+for i in map(list, zip(*Mat1)):
+    print(i)
